b4a1aaec6bc1f0e57a72da1ab90e488008b51fae
jmlemaster86 <jon.m.lemaster-1@ou.edu>
2020-02-29 16:03:44 -0600

    Added listeners, changed the layout constraints.

--- app/src/main/java/com/example/supersudoku/MainActivity.java
+++ app/src/main/java/com/example/supersudoku/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.supersudoku;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
+    TextView tView;
+    Board
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_gameboard);
+        btn = (Button)findViewById(R.id.keypad1);
+        tView = (TextView)findViewById(R.id.textView);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(tView.isSelected()){
+                    tView.setText("1");
+                }
+
+            }
+        });
+
+        tView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tView.setSelected(true);
+            }
+        });
+
     }
+
+
+
+
 }
